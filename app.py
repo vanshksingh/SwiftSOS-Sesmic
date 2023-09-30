f19892fa0e09456aacbf4631d6ece96fb05f4aea
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2023-10-01 04:03:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ except requests.exceptions.RequestException as expt:
     st.stop()
 
 st.set_page_config(
-    page_title="QuakeEye",
+    page_title="SwiftSOS",
     page_icon="https://cdn-icons-png.flaticon.com/512/2377/2377860.png",
  
 )
